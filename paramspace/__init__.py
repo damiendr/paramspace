9cb8d61fcaad73536a3f7f22a577a0934dc03be8
Damien Drix <damien.drix@gmail.com>
2014-12-28 17:33:56 +0100

    convenient imports in __init__.py

---
+++ paramspace/__init__.py
@@ -0,0 +1,5 @@
+
+
+from paramspace.parameters import sample, load_param
+from paramspace.expressions import (choice, pchoice, randint, uniform, loguniform, normal, lognormal, quniform, qloguniform, qnormal, qlognormal)
+
