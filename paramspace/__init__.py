70559acac70a8c2bf9406fe81fe265af3f678d07
Damien Drix <damien.drix@gmail.com>
2015-01-08 11:03:29 +0100

    readme

--- paramspace/__init__.py
+++ paramspace/__init__.py
@@ -1,7 +1,5 @@
 
 
 from paramspace.parameters import sample, load_param, load_model
-
-#from paramspace.expressions import (to_hp, choice, pchoice, randint, uniform, loguniform, normal, lognormal, quniform, qloguniform, qnormal, qlognormal)
-
 from paramspace.pyll import *
+
